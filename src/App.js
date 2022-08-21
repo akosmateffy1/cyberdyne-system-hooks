704a18641ee66b80896802b8a7a1f9c574895ba0
Ákos Mátéffy <102735443+akosmateffy1@users.noreply.github.com>
2022-08-21 23:34:34 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component, useState, useEffect } from "react";
+import TerminatorList from "./components/terminator-list/terminator-list.component";
+
+import SearchBox from "./components/searchbox/searchbox.component";
+
+import './App.css';
+
+function App(){
+ const [models, setModels] = useState([]);
+ const [searchfield, setSearchfield] = useState('');
+ const [szamlalo, setSzamlalo] = useState(0);
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+  };
+
+
+  useEffect(() => {
+  console.log(szamlalo);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setModels(users))},
+      [szamlalo]
+    );
+
+
+    const filteredModels = models.filter((model) => {
+      return model.name
+        .toLowerCase()
+        .includes(searchfield.toLowerCase());
+    });
+
+    if(models.length === 0) {
+      return <h1>Betöltés folyamatban...</h1>
+    }
+    return (
+      <div className="tc">
+        <h1 className="f1">Terminator modellek</h1>
+        <button onClick={() => setSzamlalo(szamlalo+1)}>Kattints ide!</button>
+        <SearchBox searchChange={onSearchChange} />
+        <TerminatorList models={filteredModels} />
+      </div>
+    );
+  }
+
+export default App;
